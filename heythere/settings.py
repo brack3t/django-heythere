f5ddb5bc2c768adb60f6b498f5f6e71c5d195095
Kenneth Love <kenneth@gigantuan.net>
2014-03-07 15:59:34 -0800

    that's a silly setting

--- heythere/settings.py
+++ heythere/settings.py
@@ -3,7 +3,6 @@ from django.conf import settings as django_settings
 BASIC_NOTIFICATIONS = {
     'DEFAULT': {
         'persistant': True,  # stays until dismissed
-        'send_onsite': True,  # send as message on site
         'send_as_email': False,  # send as email
         'headline_template': '{{headline}}',  # Django template for headline
         'body_template': '{{body}}',  # Django template for body
